27253195e9dd326562fbbbc02549961b91bd9836
Jonathan Ballet <jon@multani.info>
2024-04-28 16:52:56 +0200

    Output more workspace related settings

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+output "organization" {
+  value = tfe_workspace.this.organization
+}
+
+output "project_id" {
+  value = tfe_workspace.this.project_id
+}
+
 output "workspace_id" {
   value = tfe_workspace.this.id
 }
